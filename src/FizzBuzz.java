fef2c5c691e4eb181d711cfaf43ddeac5f22d13e
xiran <xiran.yin@mail.utoronto.ca>
2024-09-09 18:39:42 -0400

    hhh

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,31 +8,36 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-                i++;
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-                i++;
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-                i++;
-
-            } else {
-
-                System.out.println(i);
-                i++;
-            }
+            i = getAnInt(i);
         }
+    }
+
+    public static int getAnInt(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+            i++;
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+            i++;
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+            i++;
+
+        } else {
+
+            System.out.println(i);
+            i++;
+        }
+        return i;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.getAnInt(5);
+        FizzBuzz.main(args);
+    }
+}
